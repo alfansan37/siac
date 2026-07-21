b850f7c00a2e205e1afe23d5e6cbd11116c21b35
alfansan37 <37alfani@gmail.com>
2026-07-21 09:54:30 +0700

    Add session check for admin login redirection

---
+++ index1.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ . '/config/app.php';
+
+// Cek apakah admin sudah login atau belum
+if (isset($_SESSION['user_id'])) {
+    // Jika sudah login, langsung arahkan ke Dashboard
+    header("Location: " . BASE_URL . "/modules/dashboard/index.php");
+    exit;
+} else {
+    // Jika belum login, arahkan ke halaman Login
+    header("Location: " . BASE_URL . "/modules/auth/login.php");
+    exit;
+}
+?>
